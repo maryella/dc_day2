589a1668d5b8aa7290c10fb83a1766e2523c4c73
Da-Colon <dcolonantonio@gmail.com>
2020-01-17 01:14:41 -0500

    intial commit

---
+++ exercises.py
@@ -0,0 +1,21 @@
+# 1. Hello, you!
+
+# name = input('What is your name? ')
+# print('Hello ' + name + '!')
+
+# ------------------------#
+
+# 2. HELLO, YOU!
+
+# name = input('What is your name? ').upper()
+# length = len(name)
+# print('HELLO ' + name + '! ' + ' YOUR NAME HAS ' + str(length) + ' LETTERS IN IT!' )
+# print('HELLO %s! YOUR NAME HAS %s LETTERS IN IT!' % (name, length))
+
+#-------------------------#
+
+# 3. Madlib
+# print('Please fill in the blanks below: ___(name)___\'s favorite ___(subject)___ in school. ')
+# name = input('Enter a name ').upper()
+# subject = input(name + '\'s' + ' favorite school subject? ').upper()
+# print('%s\'s favorite subject in school is %s.' % (name, subject))
